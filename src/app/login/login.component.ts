0ccd2e657fde22e105b326293625d98d2129d37e
kushvlshresthv <kushvlshresthv@gmail.com>
2025-09-25 17:26:14 +0545

    Big Bang

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit {
+  router = inject(Router);
+  http = inject(HttpClient);
+
+  formData =  new FormGroup({
+      username: new FormControl("", {
+        validators: [Validators.required],
+      }),
+      password: new FormControl("", {
+        validators: [Validators.required],
+      })
+    }
+  );
+
+  username!: FormControl;
+  password!: FormControl;
+
+  ngOnInit(): void {
+    this.username = this.formData.controls.username;
+    this.password = this.formData.controls.password;
+  }
+
+  onSubmit() {
+    console.log("form has been submitted");
+  }
+
+}
