584d894ce6d6b3835f4e49dd152e9f06336696e3
kushvlshresthv <kushvlshresthv@gmail.com>
2025-10-08 19:55:15 +0545

    made the header, and sidebar fixed to the ui

--- src/app/committee-details/committee-details.component.ts
+++ src/app/committee-details/committee-details.component.ts
@@ -2,15 +2,158 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { BACKEND_URL } from '../../global_constants';
-import { CommitteeDetailsDto } from '../models/models';
+import { CommitteeDetailsDto, MemberSummaryDto } from '../models/models';
 import { Response } from '../response/response';
+import { MemberSummariesComponent } from './committee-overview/member-summaries/member-summaries.component';
 
 @Component({
   selector: 'app-committee-details',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MemberSummariesComponent],
   templateUrl: './committee-details.component.html',
   styleUrl: './committee-details.component.scss',
 })
 export class CommitteeDetailsComponent{
+  committeeMembers: MemberSummaryDto[] = [];
+  ngOnInit(): void {
+    //TODO: fetch committee members from backend
+
+    this.committeeMembers.push({
+      memberId: 1,
+      firstName: "Ram Bahadur Shrestha",
+      lastName: "Sharma",
+      institution: "Kathmandu University",
+      post: "President",
+      role: "Admin"
+      })
+
+    this.committeeMembers.push({
+        memberId: 2,
+        firstName: "Shyam",
+        lastName: "Thapa",
+        institution: "Tribhuvan University",
+        post: "Member",
+        role: "User"
+        })
+
+    this.committeeMembers.push({
+      memberId: 3,
+      firstName: "Hari",
+      lastName: "Gurung",
+      institution: "Pokhara University",
+      post: "Secretary",
+      role: "User"
+      })
+
+    this.committeeMembers.push({
+      memberId: 4,
+      firstName: "Sita",
+      lastName: "Kumari",
+      institution: "Patan Academy of Health Sciences",
+      post: "Member",
+      role: "User"
+      })
+
+    this.committeeMembers.push({
+      memberId: 5,
+      firstName: "Gita",
+      lastName: "Rai",
+      institution: "B.P. Koirala Institute of Health Sciences",
+      post: "Member",
+      role: "User"
+      })
+
+
+    this.committeeMembers.push({
+      memberId: 1,
+      firstName: "Ram Bahadur Shrestha",
+      lastName: "Sharma",
+      institution: "Kathmandu University",
+      post: "President",
+      role: "Admin"
+      })
+
+    this.committeeMembers.push({
+        memberId: 2,
+        firstName: "Shyam",
+        lastName: "Thapa",
+        institution: "Tribhuvan University",
+        post: "Member",
+        role: "User"
+        })
+
+    this.committeeMembers.push({
+      memberId: 3,
+      firstName: "Hari",
+      lastName: "Gurung",
+      institution: "Pokhara University",
+      post: "Secretary",
+      role: "User"
+      })
+
+    this.committeeMembers.push({
+      memberId: 4,
+      firstName: "Sita",
+      lastName: "Kumari",
+      institution: "Patan Academy of Health Sciences",
+      post: "Member",
+      role: "User"
+      })
+
+    this.committeeMembers.push({
+      memberId: 5,
+      firstName: "Gita",
+      lastName: "Rai",
+      institution: "B.P. Koirala Institute of Health Sciences",
+      post: "Member",
+      role: "User"
+
+      })
+
+
+    this.committeeMembers.push({
+      memberId: 1,
+      firstName: "Ram Bahadur Shrestha",
+      lastName: "Sharma",
+      institution: "Kathmandu University",
+      post: "President",
+      role: "Admin"
+      })
+
+    this.committeeMembers.push({
+        memberId: 2,
+        firstName: "Shyam",
+        lastName: "Thapa",
+        institution: "Tribhuvan University",
+        post: "Member",
+        role: "User"
+        })
+
+    this.committeeMembers.push({
+      memberId: 3,
+      firstName: "Hari",
+      lastName: "Gurung",
+      institution: "Pokhara University",
+      post: "Secretary",
+      role: "User"
+      })
+
+    this.committeeMembers.push({
+      memberId: 4,
+      firstName: "Sita",
+      lastName: "Kumari",
+      institution: "Patan Academy of Health Sciences",
+      post: "Member",
+      role: "User"
+      })
+
+    this.committeeMembers.push({
+      memberId: 5,
+      firstName: "Gita",
+      lastName: "Rai",
+      institution: "B.P. Koirala Institute of Health Sciences",
+      post: "Member",
+      role: "User"
+      })
+  }
 }
