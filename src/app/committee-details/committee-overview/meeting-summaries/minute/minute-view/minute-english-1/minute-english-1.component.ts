57e11886e3b0b02f7c217eeccd35e92a414f24f0
kushvlshresthv <kushvlshresthv@gmail.com>
2025-10-18 11:57:50 +0545

    english minute set up done

---
+++ src/app/committee-details/committee-overview/meeting-summaries/minute/minute-view/minute-english-1/minute-english-1.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { MinuteDataDto } from '../../../../../../models/models';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-minute-english-1',
+  standalone: true,
+  imports: [DatePipe],
+  templateUrl: './minute-english-1.component.html',
+  styleUrl: './minute-english-1.component.scss'
+})
+export class MinuteEnglish1Component {
+  minuteData = input.required<MinuteDataDto>();
+}
--- src/app/committee-details/committee-overview/meeting-summaries/minute/minute-view/minute-view.component.ts
+++ src/app/committee-details/committee-overview/meeting-summaries/minute/minute-view/minute-view.component.ts
@@ -3,11 +3,12 @@ import { SafeHtml } from '@angular/platform-browser';
 import { MinuteDataDto } from '../../../../../models/models';
 import { DatePipe } from '@angular/common';
 import { MinuteNepali1Component } from './minute-nepali-1/minute-nepali-1.component';
+import { MinuteEnglish1Component } from './minute-english-1/minute-english-1.component';
 
 @Component({
   selector: 'app-minute-view',
   standalone: true,
-  imports: [DatePipe, MinuteNepali1Component],
+  imports: [DatePipe, MinuteNepali1Component, MinuteEnglish1Component],
   templateUrl: './minute-view.component.html',
   styleUrl: './minute-view.component.scss'
 })
