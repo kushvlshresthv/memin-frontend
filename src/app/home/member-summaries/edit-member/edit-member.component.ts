121e55c45218d14ac087b8be9d7a61984a9f6c7e
kushvlshresthv <kushvlshresthv@gmail.com>
2025-12-06 14:47:20 +0545

    * [edit-member] integrated /updateMember feature with backend

--- src/app/home/member-summaries/edit-member/edit-member.component.ts
+++ src/app/home/member-summaries/edit-member/edit-member.component.ts
@@ -1,9 +1,13 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MemberCreationDto, MemberDetailsDto, MemberFormData } from '../../../models/models';
+import {
+  MemberCreationDto,
+  MemberDetailsDto,
+  MemberFormData,
+} from '../../../models/models';
 import { BACKEND_URL } from '../../../../global_constants';
-import { Response } from '../../../response/response'
+import { Response } from '../../../response/response';
 import { MemberFormComponent } from '../../../forms/member-form/member-form.component';
 
 @Component({
@@ -11,57 +15,84 @@ import { MemberFormComponent } from '../../../forms/member-form/member-form.comp
   standalone: true,
   imports: [MemberFormComponent],
   templateUrl: './edit-member.component.html',
-  styleUrl: './edit-member.component.scss'
+  styleUrl: './edit-member.component.scss',
 })
-export class EditMemberComponent implements OnInit{
-
-  constructor(private httpClient: HttpClient, private activatedRoute: ActivatedRoute) {
-    console.log("EditMemberComponent constructor called");
+export class EditMemberComponent implements OnInit {
+  constructor(
+    private httpClient: HttpClient,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) {
+    console.log('EditMemberComponent constructor called');
   }
-
 
   hasDataLoaded = false;
   httpParams!: HttpParams;
 
   memberFormData: MemberFormData = {
-    firstName: '' ,
+    firstName: '',
     lastName: '',
     title: '',
-    post: ''
-  }
+    post: '',
+  };
 
   ngOnInit() {
-    console.log("trying to load data");
-    this.activatedRoute.queryParams.subscribe((receivedParams)=> {
-      this.httpParams = new HttpParams().set('memberId', receivedParams['memberId']);
+    console.log('trying to load data');
+    this.activatedRoute.queryParams.subscribe((receivedParams) => {
+      this.httpParams = new HttpParams().set(
+        'memberId',
+        receivedParams['memberId'],
+      );
 
-      this.httpClient.get<Response<MemberDetailsDto>>(BACKEND_URL + '/api/getMemberDetails', {
-	withCredentials: true,
-	params: this.httpParams,
-      }).subscribe({
-	next: (response)=> {
-	  const mainBody = response.mainBody;
-	  this.memberFormData.firstName = mainBody.firstName;
-	  this.memberFormData.lastName = mainBody.lastName;
-	  this.memberFormData.title = mainBody.title;
-	  this.memberFormData.post = mainBody.post;
-	  this.hasDataLoaded = true;
-	  console.log("enjoy");
-	},
-	error: (error)=> {
+      this.httpClient
+        .get<Response<MemberDetailsDto>>(
+          BACKEND_URL + '/api/getMemberDetails',
+          {
+            withCredentials: true,
+            params: this.httpParams,
+          },
+        )
+        .subscribe({
+          next: (response) => {
+            const mainBody = response.mainBody;
+            this.memberFormData.firstName = mainBody.firstName;
+            this.memberFormData.lastName = mainBody.lastName;
+            this.memberFormData.title = mainBody.title;
+            this.memberFormData.post = mainBody.post;
+            this.hasDataLoaded = true;
+            console.log('enjoy');
+          },
+          error: (error) => {
             console.log('todo: show error', error);
-	}
-      })
-    })
+          },
+        });
+    });
   }
-
-
 
   onFormSave(requestBody: MemberCreationDto) {
+    console.log('submitting the form');
 
+    this.activatedRoute.queryParams.subscribe((receivedParams) => {
+      this.httpParams = new HttpParams().set(
+        'memberId',
+        receivedParams['memberId'],
+      );
+
+      this.httpClient
+        .patch<
+          Response<Object>
+        >(BACKEND_URL + '/api/updateMember', requestBody, { withCredentials: true, params: this.httpParams })
+        .subscribe({
+          next: (response) => {
+            console.log('TODO: handle this response');
+            console.log(response);
+	this.router.navigate(['/home/members-list'], {queryParamsHandling:'preserve'})
+          },
+          error: (error) => {
+            console.log('TODO: handle this error');
+            console.log(error);
+          },
+        });
+    });
   }
 }
-
-
-
-
