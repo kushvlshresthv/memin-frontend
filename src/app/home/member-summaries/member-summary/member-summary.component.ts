1003c166b55bd4ad971fb52c868ad0539412a8f6
kushvlshresthv <kushvlshresthv@gmail.com>
2025-12-06 15:37:33 +0545

    *[member-summary] added the options menu for summary cards

--- src/app/home/member-summaries/member-summary/member-summary.component.ts
+++ src/app/home/member-summaries/member-summary/member-summary.component.ts
@@ -1,5 +1,7 @@
 import { Component, input } from '@angular/core';
 import { MemberDetailsDto } from '../../../models/models';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-member-summary',
@@ -10,4 +12,22 @@ import { MemberDetailsDto } from '../../../models/models';
 })
 export class MemberSummaryComponent {
   memberDetail = input.required<MemberDetailsDto>();
+  showMenuOptions = false;
+  constructor (private httpClient: HttpClient, private router: Router) {
+    
+  }
+
+  toggleMenu(event: Event) {
+    event.stopPropagation();
+    this.showMenuOptions = !this.showMenuOptions;
+  }
+
+  onEditOptionClick(event: Event) {
+    event.stopPropagation();
+    this.router.navigate(['/home/members-list/edit'], {
+      queryParams: {
+        memberId: this.memberDetail().memberId,
+      },
+    });
+  }
 }
