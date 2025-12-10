72dfdeb8a77bc92fe702856e796683d077f3529a
kushvlshresthv <kushvlshresthv@gmail.com>
2025-12-10 19:46:44 +0545

    * [sidebar] condensed the sidar to icons

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -15,9 +15,8 @@ import { filter } from 'rxjs';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent implements OnInit {
-  subheading: 'main-menu' | 'committee-menu' | 'login' = 'main-menu';
-  constructor(private router: Router, private route: ActivatedRoute) {}
+export class SidebarComponent {
+  constructor( private route: ActivatedRoute) {}
 
   // Method to get only the committeeId query parameter and ignoring other query parameters if any
   getCommitteeQueryParams(): { [key: string]: any } {
@@ -25,24 +24,5 @@ export class SidebarComponent implements OnInit {
     return currentParams['committeeId']
       ? { committeeId: currentParams['committeeId'] }
       : {};
-  }
-
-  ngOnInit(): void {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        if (event.urlAfterRedirects.startsWith('/committee-details')) {
-          this.subheading = 'committee-menu';
-        } else if (event.urlAfterRedirects === '/login') {
-          this.subheading = 'login';
-        } else if (
-          event.urlAfterRedirects === '/' ||
-          event.urlAfterRedirects.startsWith('/home')
-        ) {
-          this.subheading = 'main-menu';
-        } else {
-          this.subheading = 'main-menu'; //default
-        }
-      });
   }
 }
