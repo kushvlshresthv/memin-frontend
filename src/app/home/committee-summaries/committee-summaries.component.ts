e3a29fbc22659fb9d28eac5e06cb179176091f7c
kushvlshresthv <kushvlshresthv@gmail.com>
2025-12-10 21:08:59 +0545

    * [committee-summaries] implemented committee sort dropdown

--- src/app/home/committee-summaries/committee-summaries.component.ts
+++ src/app/home/committee-summaries/committee-summaries.component.ts
@@ -5,32 +5,56 @@ import { CommitteeSummary } from './committee-summary/committee-summary.model';
 import { Response } from '../../response/response';
 import { CommitteeSummaryComponent } from './committee-summary/committee-summary.component';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-committee-summaries',
   standalone: true,
-  imports: [CommitteeSummaryComponent, RouterLink],
+  imports: [CommitteeSummaryComponent, RouterLink, FormsModule],
   templateUrl: './committee-summaries.component.html',
   styleUrl: './committee-summaries.component.scss',
 })
 export class CommitteeSummariesComponent {
   committeeSummaries!: CommitteeSummary[];
+  sortBy: 'createdDate' | 'title' = 'createdDate';
   constructor(private httpClient: HttpClient) {}
-  hasCommitteesLoaded=false;
+  hasCommitteesLoaded = false;
 
   ngOnInit(): void {
     this.httpClient
-      .get<Response<CommitteeSummary[]>>(BACKEND_URL + '/api/getMyActiveCommittees', {
-        withCredentials: true,
-      })
+      .get<Response<CommitteeSummary[]>>(
+        BACKEND_URL + '/api/getMyActiveCommittees',
+        {
+          withCredentials: true,
+        }
+      )
       .subscribe({
         next: (response) => {
           this.committeeSummaries = response.mainBody;
-	  this.hasCommitteesLoaded = true;
+          this.sortCommittees();
+          this.hasCommitteesLoaded = true;
         },
         error: (response) => {
           console.log('error fetching committee');
         },
       });
+  }
+
+  onSortChange(event: Event): void {
+    this.sortCommittees();
+  }
+
+  private sortCommittees(): void {
+    if (!this.committeeSummaries) return;
+
+    this.committeeSummaries.sort((a, b) => {
+      if (this.sortBy === 'createdDate') {
+        return (
+          new Date(b.createdDate).getTime() - new Date(a.createdDate).getTime()
+        );
+      } else {
+        return a.name.localeCompare(b.name);
+      }
+    });
   }
 }
