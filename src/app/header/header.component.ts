4ee74c203982329c430396ad5a07002a123b6472
kushvlshresthv <kushvlshresthv@gmail.com>
2025-12-13 21:13:23 +0545

    * [header] fixed the header css and added logo link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { GlobalSearchComponent } from './global-search/global-search.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [GlobalSearchComponent],
+  imports: [GlobalSearchComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
