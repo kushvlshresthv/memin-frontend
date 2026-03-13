1dcd301e94c82e6ffd0b857b18ef6677f57b07c2
kushvlshresthv <kushvlshresthv@gmail.com>
2026-03-13 19:29:57 +0545

    * fixed the home page loader and heading position bug

--- src/global_constants.ts
+++ src/global_constants.ts
@@ -1,2 +1,3 @@
 // export const BACKEND_URL = 'https://memin-backend-kp0y.onrender.com';
-export const BACKEND_URL = 'http://localhost:8080';
+// export const BACKEND_URL = 'http://localhost:8080';
+export const BACKEND_URL = 'https://lcwgko00wg0soc4k0kgo8sko.itclub.asmitphuyal.com.np';
