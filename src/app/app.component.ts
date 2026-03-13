9d7a4a8e28714b09a4d292c1d01ca53ee4a416cb
kushvlshresthv <kushvlshresthv@gmail.com>
2026-03-13 20:11:15 +0545

    * added shortcuts to various functionalities in the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,50 @@
-import { Component } from '@angular/core';
-import {  RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { AuthService } from './service/auth.service';
 import { PopupComponent } from './popup/popup.component';
+import { BACKEND_URL } from '../global_constants';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent /*DemoComponent*/, PopupComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent /*DemoComponent*/,
+    PopupComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   isLoggedIn = false;
 
-  constructor(private authService: AuthService) {
-    this.authService.loggedIn$.subscribe(value=> this.isLoggedIn = value)
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.authService.loggedIn$.subscribe((value) => (this.isLoggedIn = value));
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    // Using Alt + N instead to avoid browser conflict
+    const isAltPressed = event.altKey;
+    const isNPressed = event.code === 'KeyN';
+    const isMPressed = event.code == 'KeyM';
+    const isCPressed = event.code == 'KeyC';
+
+    if (isAltPressed && isNPressed) {
+      event.preventDefault();
+      this.router.navigate(['/home/create-meeting']);
+    } else if (isAltPressed && isMPressed) {
+      event.preventDefault();
+      this.router.navigate(['/home/create-member']);
+    } else if (isAltPressed && isCPressed) {
+      event.preventDefault();
+      this.router.navigate(['/home/create-committee']);
+    }
   }
 }
